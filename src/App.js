cdc6deed5ca45904d3374f1e7a6c220edf7bc2d0
Niharika222 <hasijaniharika8@gmail.com>
2022-02-06 19:04:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
 
-export default App;
+export default class App extends React.Component{
+
+  state={
+    details:[ {name:"Sagar",location:"Kaithal"},
+              {name:"Rahul",location:"Chandigarh"},
+              {name:"Rahul KL",location:"Banglore"},
+              {name:"Sant",location:"Yammuna Nagar"},
+              {name:"Sumit",location:"Chandigarh"},
+              {name:"Ritvik",location:"Kaithal"},
+              {name:"Amit",location:"Karnal"}
+            ]
+  };
+
+  arr = this.state.details;
+
+  search = (e) =>{
+    let value = e.target.value.trim().toLowerCase();
+
+    if(this.state.value!=""){
+      
+      let arr2 = this.arr.filter(function(res){
+        if(res.name.toLowerCase().indexOf(value) > -1)
+          return true;
+      }) 
+
+      this.setState({details:arr2});
+    }else{
+      this.setState({details:this.arr});
+    }
+    
+  }
+
+  render(){
+    return(
+      <>
+      <input type="text" placeholder="Search Your Restraunts" className="serach-box" onKeyUp={this.search}/><br/>
+      <table className="container">
+        <thead><th>Name</th><th>Location</th></thead>
+        <tbody>
+        {
+          this.state.details.map(function(data){
+            return <tr><td>{data.name}</td><td>{data.location}</td></tr>;
+          })
+        }
+        </tbody>
+        
+      </table>
+      </>
+    );
+  }
+}
